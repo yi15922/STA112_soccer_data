564324b172f73e14be809eb476b8364018b2e726
Joe Zuo <yuncong.zuo@duke.edu>
2018-11-27 05:02:19 +0000

    Extended data

--- proposal/proposal.md
+++ proposal/proposal.md
@@ -65,19 +65,20 @@ market value.
 
 ### Summary Statistics
 
-    ## # A tibble: 10 x 2
+    ## # A tibble: 11 x 2
     ##    position               n
     ##    <fct>              <int>
-    ##  1 Attacking Midfield     6
-    ##  2 Central Midfield       9
-    ##  3 Centre-Back            2
-    ##  4 Centre-Forward        10
-    ##  5 Defensive Midfield     3
-    ##  6 Goalkeeper             3
-    ##  7 Left Winger            8
-    ##  8 Left-Back              2
-    ##  9 Right Winger           5
-    ## 10 Second Striker         2
+    ##  1 Attacking Midfield     8
+    ##  2 Central Midfield      13
+    ##  3 Centre-Back           12
+    ##  4 Centre-Forward        18
+    ##  5 Defensive Midfield     4
+    ##  6 Goalkeeper             7
+    ##  7 Left Winger           16
+    ##  8 Left-Back              4
+    ##  9 Right Winger          11
+    ## 10 Right-Back             4
+    ## 11 Second Striker         3
 
 We can see that the most common position among the top 50 players is
 Centre-Forward. Most players are attackers or midfielders instead of
@@ -86,10 +87,10 @@ defenders.
     ## # A tibble: 4 x 2
     ##   age_range     n
     ##   <chr>     <int>
-    ## 1 21-25        19
-    ## 2 26-30        27
-    ## 3 above 30      3
-    ## 4 under 20      1
+    ## 1 21-25        41
+    ## 2 26-30        51
+    ## 3 above 30      4
+    ## 4 under 20      4
 
 We have tried to classify the players by age. Most players age between
 26 and 30. There are 3 players above 30 years old and there’s also one
@@ -97,7 +98,7 @@ player who is younger than
     20.
 
     ##   mean_matches sd_matches mean_goals sd_goals mean_assists sd_assists
-    ## 1        17.34    3.51446       5.22 4.514331         3.04   2.725241
+    ## 1        18.09   3.979988       4.33 4.124465         2.74   2.615494
 
 The average matches played during season 2018-2019 thus far by these top
 50 players is 17.34 and the standard deviation is 3.51. The average
@@ -106,15 +107,16 @@ goals scored during season 2018-2019 thus far by these top 50 players is
 season 2018-2019 thus far by these top 50 players is 3.04 and the
 standard deviation is 2.73.
 
-    ## # A tibble: 6 x 2
+    ## # A tibble: 7 x 2
     ##   yellow_cards     n
     ##          <int> <int>
-    ## 1            0    12
-    ## 2            1    15
-    ## 3            2     9
-    ## 4            3     4
-    ## 5            4     7
-    ## 6            5     3
+    ## 1            0    19
+    ## 2            1    27
+    ## 3            2    21
+    ## 4            3    13
+    ## 5            4    11
+    ## 6            5     7
+    ## 7            6     2
 
 We can see that until now, 38 among the 50 players have got yellow cards
 during season 2018-2019, and the maximum yellow card number is 5.
@@ -123,13 +125,16 @@ during season 2018-2019, and the maximum yellow card number is 5.
     ## 1     Kylian Mbappé         1
     ## 2 Cristiano Ronaldo         1
     ## 3   Marcus Rashford         1
+    ## 4     Douglas Costa         1
+    ## 5   Gonzalo Higuain         1
+    ## 6       Richarlison         1
 
 In season 2018-2019 thus far, there are 3 players who have already
 gotten a red card in a match, meaning they are to be expelled for the
 game: Kylian Mbappé, Cristiano Ronaldo, and Marcus Rashford.
 
     ##   max_value min_value
-    ## 1       180        65
+    ## 1       180        45
 
     ##   value_above_100m
     ## 1               13
@@ -164,20 +169,20 @@ Here we will glimpse the dataframe and view its dimensions. The
 dimensions of our data frame, with the addition of the age range
 variable, are 50 observations x 13 variables.
 
-    ## Observations: 50
+    ## Observations: 100
     ## Variables: 13
     ## $ name            <fct> Kylian Mbappé, Neymar, Lionel Messi, Mohamed S...
     ## $ position        <fct> Right Winger, Left Winger, Right Winger, Right...
     ## $ age             <int> 19, 26, 31, 26, 25, 27, 27, 26, 27, 25, 22, 25...
-    ## $ matches         <int> 18, 21, 13, 17, 21, 22, 5, 20, 20, 18, 13, 19,...
-    ## $ goals           <int> 15, 16, 14, 8, 11, 8, 0, 6, 10, 6, 2, 9, 9, 10...
+    ## $ matches         <int> 19, 22, 14, 18, 22, 24, 5, 20, 21, 19, 14, 20,...
+    ## $ goals           <int> 15, 16, 14, 9, 12, 8, 0, 6, 10, 7, 3, 9, 10, 1...
     ## $ own_goals       <int> 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0...
-    ## $ assists         <int> 8, 13, 6, 4, 4, 4, 1, 4, 5, 3, 1, 2, 6, 2, 7, ...
-    ## $ yellow_cards    <int> 4, 5, 1, 0, 4, 1, 1, 1, 1, 2, 0, 2, 0, 0, 2, 4...
+    ## $ assists         <int> 8, 13, 7, 4, 4, 5, 1, 4, 5, 3, 2, 2, 6, 2, 7, ...
+    ## $ yellow_cards    <int> 4, 5, 1, 0, 4, 3, 1, 1, 2, 2, 0, 2, 0, 0, 2, 5...
     ## $ red_cards       <int> 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0...
-    ## $ substituted_on  <int> 3, 1, 1, 1, 1, 0, 3, 4, 5, 3, 3, 3, 0, 2, 7, 3...
-    ## $ substituted_off <int> 2, 5, 1, 6, 4, 8, 2, 10, 6, 6, 3, 2, 0, 3, 5, ...
+    ## $ substituted_on  <int> 3, 1, 1, 1, 1, 0, 3, 4, 5, 4, 3, 3, 0, 3, 7, 3...
+    ## $ substituted_off <int> 3, 6, 1, 7, 4, 9, 2, 10, 6, 6, 4, 2, 0, 4, 6, ...
     ## $ market_value    <int> 180, 180, 180, 150, 150, 150, 150, 150, 150, 1...
     ## $ age_range       <chr> "under 20", "26-30", "above 30", "26-30", "21-...
 
-    ## [1] 50 13
+    ## [1] 100  13
--- web_scrape.R
+++ web_scrape.R
@@ -89,32 +89,62 @@ players1 <- tibble(
 name2 <- c("Raphael Varane", "Roberto Firmino", "Jan Oblak", "James Rodriguez", "Marc-Andre ter Stegen", " Christian Eriksen",
            "Sergio Busquets", "Robert Lewandowski", "Toni Kroos", "Sergio Aguero", "Pierre-Emerick Aubameyang", "Thomas Lemar",
            "Sadio Mane", "Samuel Umtiti", "Marco Verratti", "Koke", "Jordi Alba", "David de Gea", "Marcelo", "Luis Suarez",
-           "Miralem Pjanic", "Ivan Rakitic", "Casemiro", "Naby Keita", "Marcus Rashford")
+           "Miralem Pjanic", "Ivan Rakitic", "Casemiro", "Naby Keita", "Marcus Rashford", "Nabil Fekir", "Lorenzo Insigne",
+           "Thibaut Courtois", "Alexandre Lacazette", "Bernardo Silva", "Ederson", "Anthony Martial", "Riyad Mahrez", "Timo Werner", 
+           "Virgil van Dijk", "Daniel Carvajal", "Alvaro Morata", "Alisson", "Jorginho", "Kalidou Koulibaly", "Thiago", "Edinson Cavani",
+           "Diego Costa", "Kai Havertz", "John Stones", "Marquinhos", "Joshua Kimmich", "Sergi Roberto", "Douglas Costa", "Alexis Sanchez",
+           "Mats Hummels", "Gonzalo Higuain", "Arthur", "Federico Chiesa", "Matthijs de Ligt", "Lucas Hernandez", "Jose Gimenez", "Kingsley Coman",
+           "Milan Skriniar", "Kepa", "Fred", "Florian Thauvin", "Adrien Rabiot", "Aymeric Laporte", "Niklas Sule", "Rodrigo", "Ciro Immobile",
+           "Kyle Walker", "Heung-min Son", "David Alaba", "Thomas Muller", "Willian", "Jadon Sancho", "Leon Bailey", "Richarlison")
 
 position2 <- c("Centre-Back", "Second Striker", "Goalkeeper", "Attacking Midfield", "Goalkeeper", "Attacking Midfield", "Defensive Midfield",
                "Centre-Forward", "Central Midfield", "Centre-Forward", "Centre-Forward", "Left Winger", "Left Winger", "Centre-Back",
                "Central Midfield", "Central Midfield", "Left-Back", "Goalkeeper", "Left-Back", "Centre-Forward", "Central Midfield", "Central Midfield",
-               "Defensive Midfield", "Central Midfield", "Centre-Forward")
+               "Defensive Midfield", "Central Midfield", "Centre-Forward", "Attacking Midfield", "Left Winger", "Goalkeeper", "Centre-Forward", "Right Winger",
+               "Goalkeeper", "Left Winger", "Right Winger", "Centre-Forward", "Centre-Back", "Right-Back", "Centre-Forward", "Goalkeeper", "Defensive Midfield", "Centre-Back",
+               "Central Midfield", "Centre-Forward", "Centre-Forward", "Attacking Midfield", "Centre-Back", "Centre-Back", "Right-Back", "Right-Back", "Right Winger",
+               "Left Winger", "Centre-Back", "Centre-Forward", "Central Midfield", "Right Winger", "Centre-Back", "Left-Back", "Centre-Back", "Left Winger", "Centre-Back",
+               "Goalkeeper", "Central Midfield", "Right Winger", "Central Midfield", "Centre-Back", "Centre-Back", "Centre-Forward", "Centre-Forward", "Right-Back", "Left Winger",
+               "Left-Back", "Second Striker", "Right Winger", "Left Winger", "Left Winger", "Left Winger")
 
-age2 <- c(25,27,25,27,26,26,30,30,28,30,29,23,26,25,26,26,29,28,30,31,28,30,26,23,21)
+age2 <- c(25,27,25,27,26,26,30,30,28,30,29,23,26,25,26,26,29,28,30,31,28,30,26,23,21,25,27,26,27,24,25,22,27,22,27,26,26,26,26,27,27,31,30,19,24,24,23,26,28,29,
+          29,30,22,21,19,22,23,22,23,24,25,25,23,24,23,27,28,28,26,26,29,30,18,21,21)
 
-matches2 <- c(17,20,17,13,18,15,21,20,19,15,16,17,16,9,15,15,18,21,10,19,20,20,21,11,19)
+matches2 <- c(17,20,17,13,18,15,21,20,19,15,16,17,16,9,15,15,18,21,10,19,20,20,21,11,19,
+              13,20,17,15,21,19,15,19,21,22,11,21,20,21,17,17,17,14,19,20,22,25,16,15,15,
+              17,13,21,19,28,16,11,3,20,19,12,17,17,18,19,22,20,20,24,21,24,23,23,16,18)
 
-goals2 <- c(0,5,0,4,0,3,0,15,2,11,9,1,7,0,0,3,2,0,3,11,3,3,1,0,5)
+goals2 <- c(0,5,0,4,0,3,0,15,2,11,9,1,7,0,0,3,2,0,3,11,3,3,1,0,5,
+            4,10,0,6,6,0,7,6,9,2,1,6,0,2,0,2,11,4,6,0,2,0,0,0,3,
+            1,7,0,2,2,0,2,1,0,0,1,11,2,2,2,3,9,1,4,1,5,3,5,1,9)
 
-own_goals2 <- c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+own_goals2 <- c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0)
 
-assists2 <- c(0,2,0,4,0,4,0,5,3,3,1,2,1,1,1,1,8,0,1,7,2,3,0,0,2)
+assists2 <- c(0,2,0,4,0,4,0,5,3,3,1,2,1,1,1,1,8,0,1,7,2,3,0,0,2,
+              4,3,0,4,4,1,0,4,2,1,2,0,0,0,1,2,4,2,7,0,1,5,4,3,3,
+              1,1,1,6,1,1,0,1,0,0,0,4,2,0,0,4,5,1,10,2,3,6,9,0,2)
 
-yellow_cards2 <- c(0,0,0,2,1,0,5,0,2,4,0,2,1,2,4,4,1,0,3,5,3,3,1,1,1)
+yellow_cards2 <- c(0,0,0,2,1,0,5,0,2,4,0,2,1,2,4,4,1,0,3,5,3,3,1,1,1,
+                   3,3,0,1,2,0,2,0,1,2,4,6,0,4,5,2,1,6,1,2,1,3,0,3,2,
+                   3,4,2,5,1,4,2,0,5,0,2,3,3,1,1,2,3,1,2,1,1,1,1,2,1)
 
-red_cards2 <- c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1)
+red_cards2 <- c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,
+                0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1)
 
-substituted_on2 <- c(1,4,0,4,0,3,2,0,0,1,4,5,0,1,1,2,1,0,0,1,0,2,2,5,6)
+substituted_on2 <- c(1,4,0,4,0,3,2,0,0,1,4,5,0,1,1,2,1,0,0,1,0,2,2,5,6,
+                     3,1,0,6,3,0,3,9,2,0,1,5,0,0,0,2,1,0,1,3,1,1,2,9,5,
+                     1,0,6,1,1,1,1,2,0,1,3,3,2,0,2,4,3,2,6,2,8,6,12,8,6)
 
-substituted_off2 <- c(2,7,0,8,0,1,5,4,2,10,9,8,5,1,9,0,0,1,2,3,13,3,7,4,8)
+substituted_off2 <- c(2,7,0,8,0,1,5,4,2,10,9,8,5,1,9,0,0,1,2,3,13,3,7,4,8,
+                      7,7,0,1,4,0,3,6,10,1,2,9,0,4,0,2,4,6,4,3,1,0,2,7,6,
+                      3,1,12,5,1,2,4,1,0,1,7,9,2,1,0,3,4,1,12,4,8,13,6,5,5)
 
-market_value2 <- c(80,80,80,80,80,80,80,80,80,80,75,70,70,70,70,70,70,70,70,70,70,70,70,65,65)
+market_value2 <- c(80,80,80,80,80,80,80,80,80,80,75,70,70,70,70,70,70,70,70,70,70,70,70,65,65,
+                   65,65,65,65,60,60,60,60,60,60,60,60,60,60,60,60,60,60,55,55,55,55,55,55,55,
+                   55,55,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,45,45,45)
 
 players2 <- tibble(
   name = name2,
